130d6da93878482dd8993fd512466c889b792da1
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-03-05 19:10:30 +0300

    added task descriptions

--- src/lesson13/classwork/Task1.kt
+++ src/lesson13/classwork/Task1.kt
@@ -1,5 +1,10 @@
 package lesson13.classwork
 
+/*
+* Задача 1:Создайте объект класса Person с полем name, используя apply.
+Затем выведите имя через let.
+* */
+
 data class Person(var name: String = "")
 
 fun main() {
--- src/lesson13/classwork/Task2.kt
+++ src/lesson13/classwork/Task2.kt
@@ -1,5 +1,10 @@
 package lesson13.classwork
 
+/*
+* Задача 2: Напишите код, который создает объект класса Book и выводит
+информацию о нем, используя run.
+* */
+
 data class Book(var name: String, var author: String)
 
 fun main() {
